932738498b15bb15c8297c490653f0f05a019bca
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-04-13 23:36:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ global.owner = [
   ['6282287750102']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = ['6282287750102'] // Want some help?
+global.prems = ['6282287750102'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
